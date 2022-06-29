f610dfceafc7cc3eda41f237f2ce79955a590c42
SergeiAndreevich <sergey.troshin.00@gmail.com>
2022-06-29 22:20:54 +0300

    try numb1

---
+++ main.js
@@ -0,0 +1,18 @@
+let numberOfFilms = prompt();
+
+let personalMovieDB: {
+	count: numberOfFilms;
+	movies: {};
+	actors: {};
+	genres : [];
+	privat: false;
+}
+
+let film = prompt('Какой фильм последний смотрели?');
+let rate = prompt('На сколько оценишь?');
+
+movies: {
+	film: rate;
+}
+
+console.log(personalMovieDB);
